4e040f638a66eb930ae7bf38f2eefd59d2ababbc
f1lander <filanderuclez@gmail.com>
2024-07-24 12:16:55 -0600

    fixes on imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { ChevronLeft, ChevronRight, File, ListFilter } from "lucide-react";
-import { useMediaQuery } from "@/hooks/use-media-query";
+import { useMediaQuery } from "@/lib/hooks";
 
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
