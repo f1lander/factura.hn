a035660473723923947b4a2525278830d21c46c8
CamiloTello002 <camilojtello5@gmail.com>
2024-09-17 20:44:41 -0500

    refactor(Navigation): Leave room for banner inside nav for non-logged users

--- components/molecules/Navigation.tsx
+++ components/molecules/Navigation.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Menu, CircleUser, FileInputIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
@@ -34,18 +34,22 @@ export function Navigation() {
   useEffect(() => {
     const fetchUser = async () => {
       setIsLoading(true);
-      const { data: { user } } = await supabase.auth.getUser();
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
       setUser(user);
       setIsLoading(false);
     };
 
     fetchUser();
 
-    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
-      console.log("Auth state changed:", event);
-      setUser(session?.user ?? null);
-      setIsLoading(false);
-    });
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        console.log("Auth state changed:", event);
+        setUser(session?.user ?? null);
+        setIsLoading(false);
+      },
+    );
 
     return () => {
       authListener.subscription.unsubscribe();
@@ -60,109 +64,115 @@ export function Navigation() {
   };
 
   return (
-    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-50">
-      <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 z-50">
-        <Link
-          href="#"
-          className="flex items-center gap-2 text-lg font-semibold md:text-base"
-        >
-          <FileInputIcon className="h-6 w-6" />
-          <span>factura.hn</span>
-        </Link>
-
-        {navItems.map((item) => (
+    <header className="stick flex flex-col">
+      {/*<header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-50">*/}
+      <div>Aquí es donde va a ir el banner</div>
+      <div className="flex top-0 h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-50">
+        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 z-50">
           <Link
-            key={item.href}
-            href={item.href}
-            className={cn(
-              "transition-colors hover:text-foreground",
-              pathname === item.href
-                ? "text-foreground"
-                : "text-muted-foreground"
-            )}
+            href="#"
+            className="flex items-center gap-2 text-lg font-semibold md:text-base"
           >
-            {item.label}
+            <FileInputIcon className="h-6 w-6" />
+            <span>factura.hn</span>
           </Link>
-        ))}
-      </nav>
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button
-            variant="outline"
-            size="icon"
-            className="shrink-0 md:hidden"
-          >
-            <Menu className="h-5 w-5" />
-            <span className="sr-only">Toggle navigation menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left">
-          <nav className="grid gap-6 text-lg font-medium">
+
+          {navItems.map((item) => (
             <Link
-              href="#"
-              className="flex items-center gap-2 text-lg font-semibold"
+              key={item.href}
+              href={item.href}
+              className={cn(
+                "transition-colors hover:text-foreground",
+                pathname === item.href
+                  ? "text-foreground"
+                  : "text-muted-foreground",
+              )}
             >
-              <FileInputIcon className="h-6 w-6" />
-              <span className="sr-only">factura.hn</span>
+              {item.label}
             </Link>
-            {navItems.map((item) => (
+          ))}
+        </nav>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="shrink-0 md:hidden"
+            >
+              <Menu className="h-5 w-5" />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <nav className="grid gap-6 text-lg font-medium">
               <Link
-                key={item.href}
-                href={item.href}
-                className={cn(
-                  "hover:text-foreground",
-                  pathname === item.href
-                    ? "text-foreground"
-                    : "text-muted-foreground"
-                )}
+                href="#"
+                className="flex items-center gap-2 text-lg font-semibold"
               >
-                {item.label}
+                <FileInputIcon className="h-6 w-6" />
+                <span className="sr-only">factura.hn</span>
               </Link>
-            ))}
-          </nav>
-        </SheetContent>
-      </Sheet>
-      <div className="profile-menu flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4 justify-end">
-        {isLoading ? (
-          <p>Cargando...</p>
-        ) : user ? (
-          <>
-            <p className="font-medium">Bienvenido, {user.user_metadata.full_name || user.email}</p>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  variant="secondary"
-                  size="icon"
-                  className="rounded-full"
-                >
-                  {user.user_metadata.avatar_url ? (
-                    <img
-                      src={user.user_metadata.avatar_url}
-                      alt="Profile"
-                      className="h-8 w-8 rounded-full"
-                    />
-                  ) : (
-                    <CircleUser className="h-5 w-5" />
+              {navItems.map((item) => (
+                <Link
+                  key={item.href}
+                  href={item.href}
+                  className={cn(
+                    "hover:text-foreground",
+                    pathname === item.href
+                      ? "text-foreground"
+                      : "text-muted-foreground",
                   )}
-                  <span className="sr-only">Toggle user menu</span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">              
-                <DropdownMenuItem>
-                  <Link href="/home/settings">Mi cuenta</Link>
-                </DropdownMenuItem>             
-                <DropdownMenuSeparator />
-                <DropdownMenuItem onClick={handleSignOut}>
-                  Cerrar Sesión
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </>
-        ) : (
-          <Link href="/auth/login" className="btn btn-ghost sign-in-btn">
-            Iniciar Sesión
-          </Link>
-        )}
+                >
+                  {item.label}
+                </Link>
+              ))}
+            </nav>
+          </SheetContent>
+        </Sheet>
+        <div className="profile-menu flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4 justify-end">
+          {isLoading ? (
+            <p>Cargando...</p>
+          ) : user ? (
+            <>
+              <p className="font-medium">
+                Bienvenido, {user.user_metadata.full_name || user.email}
+              </p>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="secondary"
+                    size="icon"
+                    className="rounded-full"
+                  >
+                    {user.user_metadata.avatar_url ? (
+                      <img
+                        src={user.user_metadata.avatar_url}
+                        alt="Profile"
+                        className="h-8 w-8 rounded-full"
+                      />
+                    ) : (
+                      <CircleUser className="h-5 w-5" />
+                    )}
+                    <span className="sr-only">Toggle user menu</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuItem>
+                    <Link href="/home/settings">Mi cuenta</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem onClick={handleSignOut}>
+                    Cerrar Sesión
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          ) : (
+            <Link href="/auth/login" className="btn btn-ghost sign-in-btn">
+              Iniciar Sesión
+            </Link>
+          )}
+        </div>
       </div>
     </header>
   );
