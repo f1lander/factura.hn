d27fa396e0db2a5954017bbbc901f41c8668e17e
f1lander <filanderuclez@gmail.com>
2024-09-18 18:00:01 -0600

    adds enhancements

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -262,7 +262,7 @@ export default function Invoices() {
 
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <div className="flex flex-col sm:gap-4 p-12">
+      <div className="flex flex-col p-6 sm:gap-4 lg:p-12">
         <main className="flex w-full flex-col items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
           <WidgetsSection />
           <InvoicesTable
--- app/page.tsx
+++ app/page.tsx
@@ -282,7 +282,7 @@ export default function Dashboard() {
 
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <div className="flex flex-col sm:gap-4 p-12">
+      <div className="flex flex-col sm:gap-4 sm:p-8 p-12">
         <main className="flex w-full flex-col items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
           <WidgetsSection />
           <InvoicesTable
