e5a53f627adab059c17644dcc3035fe8785bcda7
CamiloTello002 <camilojtello5@gmail.com>
2024-10-02 20:24:34 -0500

    feat: Make customersStore persistable

--- store/customersStore.ts
+++ store/customersStore.ts
@@ -1,12 +1,21 @@
 import { Customer } from "@/lib/supabase/services/customer";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 type CustomersStore = {
   customers: Customer[];
   setCustomers: (customers: Customer[]) => void;
 };
 
-export const useCustomersStore = create<CustomersStore>((set) => ({
-  customers: [],
-  setCustomers: (customers) => set({ customers: customers }),
-}));
+export const useCustomersStore = create<CustomersStore>()(
+  persist(
+    (set) => ({
+      customers: [],
+      setCustomers: (customers) => set({ customers: customers }),
+    }),
+    {
+      name: "isLoadedStore",
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
