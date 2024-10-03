c92b5b8f0b051ad9cff1fd13c01a31d8fee67794
CamiloTello002 <camilojtello5@gmail.com>
2024-10-02 20:22:11 -0500

    feat: Make companyStore persistable

--- store/companyStore.ts
+++ store/companyStore.ts
@@ -1,12 +1,21 @@
 import { Company } from "@/lib/supabase/services/company";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 type CompanyStore = {
   company: Company | null;
   setCompany: (company: Company) => void;
 };
 
-export const useCompanyStore = create<CompanyStore>((set) => ({
-  company: null,
-  setCompany: (company) => set({ company: company }),
-}));
+export const useCompanyStore = create<CompanyStore>()(
+  persist(
+    (set) => ({
+      company: null,
+      setCompany: (company) => set({ company: company }),
+    }),
+    {
+      name: "companyStore",
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
