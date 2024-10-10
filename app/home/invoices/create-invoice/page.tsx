5604e49a013c3d9e440b8865801ec581e1abec22
CamiloTello002 <camilojtello5@gmail.com>
2024-10-10 17:52:07 -0500

    refactor: sync invoices store when invoice is created

--- app/home/invoices/create-invoice/page.tsx
+++ app/home/invoices/create-invoice/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 import InvoiceView2 from "@/components/molecules/InvoiceView2";
 import { Invoice, invoiceService } from "@/lib/supabase/services/invoice";
+import { useInvoicesStore } from "@/store/invoicesStore";
+import { useRouter } from "next/navigation";
 export default function CreateInvoice() {
+  const { syncInvoices } = useInvoicesStore();
+  const router = useRouter();
   const handleSaveInvoice = async (invoice: Invoice) => {
     try {
       let savedInvoice: Invoice | null;
@@ -11,6 +15,9 @@ export default function CreateInvoice() {
       if (!savedInvoice) {
         throw new Error("Failed to save invoice");
       }
+      // If all went right, then sync the invoice list and redirect to next page
+      syncInvoices();
+      router.push("/home/invoices");
     } catch (error) {
       console.error("An error occurred while saving the invoice:", error);
     }
