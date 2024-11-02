d7fa238157bcbd96e90e19a7060132559ea68e32
CamiloTello002 <camilojtello5@gmail.com>
2024-11-02 17:07:15 -0500

    fix: Set cookies writing on middleware
    This update also improves on readability of the code

--- middleware.ts
+++ middleware.ts
@@ -1,51 +1,8 @@
-import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { createClient } from "@/lib/supabase/server";
+import { updateSession } from "./lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  // here we create an instance of the response
-  let response = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
-  });
-
-  const supabase = createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  const requiresCompanyData = [
-    "/home",
-    "/home/products",
-    "/home/customers",
-  ].some((path) => request.nextUrl.pathname.startsWith(path));
-
-  if (!user) {
-    if (
-      !request.nextUrl.pathname.startsWith("/auth/login") &&
-      !request.nextUrl.pathname.startsWith("/auth")
-    ) {
-      const loginUrl = new URL("/auth/login", request.url);
-      return NextResponse.redirect(loginUrl);
-    }
-  } else if (!request.nextUrl.pathname.startsWith("/home/settings")) {
-    // User is authenticated, check for company data
-    const { data: company, error } = await supabase
-      .from("companies")
-      .select("*")
-      .eq("user_id", user.id)
-      .single();
-
-    if (requiresCompanyData && company === null) {
-      const urlWithQuery = new URL("/home/settings", request.url);
-      urlWithQuery.searchParams.set("showToast", "companySetupRequired");
-      return NextResponse.redirect(urlWithQuery);
-    }
-  }
-
-  return response;
+  return await updateSession(request);
 }
 
 export const config = {
