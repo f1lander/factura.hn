2f09d5253992c14b0c8d3aea487b636eab8dc296
CamiloTello002 <camilojtello5@gmail.com>
2024-11-14 02:03:37 -0500

    feat: Don't allow invoice creation
    This will happen if the company still doesn't have products

--- components/molecules/CreateInvoiceButton.tsx
+++ components/molecules/CreateInvoiceButton.tsx
@@ -1,13 +1,27 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useProductsStore } from "@/store/productsStore";
 import { PlusCircleIcon } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { toast } from "../ui/use-toast";
+
 export default function CreateInvoiceButton() {
-  // const router = useRouter();
+  const { products } = useProductsStore();
+  const ensureProductsExistenceAndCreateInvoice = () => {
+    if (products.length < 1) {
+      return toast({
+        variant: "destructive",
+        title: "¡Aún no tenemos productos!",
+        description:
+          "Antes de poder crear facturas, debes crear al menos un producto. Ve a la pestaña de productos y agrega uno.",
+      });
+    }
+    window.location.href = "/home/invoices/create-invoice";
+  };
   return (
     <Button
       className="bg-[#00A1D4] text-white text-lg font-semibold flex gap-4 p-8"
-      // onClick={() => router.push("/home/invoices/create-invoice")}
-      onClick={() => (window.location.href = "/home/invoices/create-invoice")}
+      onClick={ensureProductsExistenceAndCreateInvoice}
     >
       <PlusCircleIcon />
       Crear factura
