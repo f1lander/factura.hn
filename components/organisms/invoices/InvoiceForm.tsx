3123a7217c04327a755910adc871ac18c1c7a4ec
CamiloTello002 <camilojtello5@gmail.com>
2024-11-16 11:25:41 -0500

    feat: Create components for invoice form and preview
    The InvoiceForm component is able to create invoices
    
    The InvoicePreview component is still in process

---
+++ components/organisms/invoices/InvoiceForm.tsx
@@ -0,0 +1,476 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { InputMask } from "@react-input/mask";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { useFieldArray, Controller, useFormContext } from "react-hook-form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Badge } from "@/components/ui/badge";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { DatePicker } from "@/components/ui/date-picker";
+import { Trash2 } from "lucide-react";
+import {
+  Invoice,
+  InvoiceItem,
+  invoiceService,
+} from "@/lib/supabase/services/invoice";
+import { numberToWords } from "@/lib/utils";
+import { toast } from "@/components/ui/use-toast";
+import { useCustomersStore } from "@/store/customersStore";
+import { useProductsStore } from "@/store/productsStore";
+import { useCompanyStore } from "@/store/companyStore";
+import { Customer, customerService } from "@/lib/supabase/services/customer";
+import { useInvoicesStore } from "@/store/invoicesStore";
+import { getStatusBadge } from "@/components/molecules/InvoicesTable";
+import { Label } from "@/components/ui/label";
+import { CustomerForm } from "@/components/molecules/CustomerForm";
+import ProductSelect from "@/components/molecules/invoiceProductInputs/ProductSelect";
+import DescriptionInput from "@/components/molecules/invoiceProductInputs/DescriptionInput";
+import QuantityInput from "@/components/molecules/invoiceProductInputs/QuantityInput";
+import DiscountInput from "@/components/molecules/invoiceProductInputs/DiscountInput";
+import UnitCostInput from "@/components/molecules/invoiceProductInputs/UnitCostInput";
+
+interface InvoiceFormProps {
+  onSave: (invoice: Invoice) => void;
+}
+
+const InvoiceForm: React.FC<InvoiceFormProps> = ({ onSave }) => {
+  const { customers, syncCustomers } = useCustomersStore();
+  const { products } = useProductsStore();
+  const { company } = useCompanyStore();
+  const { allInvoices } = useInvoicesStore();
+  const [lastInvoiceNumber, setLastInvoiceNumber] = useState<
+    string | undefined
+  >();
+  const [lastInvoiceExists, setLastInvoiceExists] = useState<boolean>(false);
+  const [isAddClientDialogOpen, setIsAddClientDialogOpen] =
+    useState<boolean>(false);
+  useEffect(() => {
+    if (allInvoices.at(-1) !== undefined) {
+      setLastInvoiceExists(true);
+    } else {
+      setLastInvoiceExists(false);
+    }
+  }, [setLastInvoiceExists, allInvoices]);
+
+  const handleFormSubmit = async (data: Partial<Customer>) => {
+    try {
+      await customerService.createCustomer(data as Customer);
+      syncCustomers();
+      setIsAddClientDialogOpen(false);
+      toast({
+        title: "Cliente creado",
+        description: "El cliente se ha guardado exitosamente.",
+      });
+    } catch (err) {
+      console.error("Error al guardar el cliente:", err);
+      // setError("No se pudo guardar el cliente. Por favor, intente de nuevo.");
+      toast({
+        title: "Error",
+        description:
+          "No se pudo guardar el cliente. Por favor, intente de nuevo.",
+        variant: "destructive",
+      });
+    }
+  };
+
+  const {
+    control,
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    getValues,
+    formState: { errors },
+    setError,
+  } = useFormContext<Invoice>();
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "invoice_items",
+  });
+  const noProductsAdded: boolean = getValues("invoice_items").length === 0;
+
+  const watchInvoiceItems = watch("invoice_items");
+
+  // this is what's consuming so much bandwidth
+  useEffect(() => {
+    const fetchData = async () => {
+      // Fetch the last invoice number
+      const lastInvoice = await invoiceService.getLastInvoice();
+      if (lastInvoice) {
+        setLastInvoiceNumber(lastInvoice);
+      } else {
+        // If no last invoice, use the range_invoice1 from company
+        setLastInvoiceNumber(company?.range_invoice1);
+      }
+    };
+
+    fetchData();
+    // como esto no ha cambiado, entonces no se ejecuta
+  }, [company]);
+
+  useEffect(() => {
+    if (lastInvoiceNumber) {
+      if (lastInvoiceExists) {
+        const nextInvoiceNumber = generateNextInvoiceNumber(lastInvoiceNumber);
+        setValue("invoice_number", nextInvoiceNumber);
+      } else if (company !== null && company.range_invoice1 !== undefined) {
+        setValue("invoice_number", company.range_invoice1);
+      }
+    }
+  }, [lastInvoiceNumber, setValue, company, lastInvoiceExists]);
+
+  useEffect(() => {
+    const subtotal = watchInvoiceItems.reduce(
+      (sum, item) => sum + (item.quantity * item.unit_cost - item.discount),
+      0,
+    );
+    const tax = subtotal * 0.15; // Assuming 15% tax rate
+    const total = subtotal + tax;
+
+    setValue("subtotal", subtotal);
+    setValue("tax", tax);
+    setValue("total", total);
+    setValue("numbers_to_letters", numberToWords(total));
+  }, [watchInvoiceItems, setValue]);
+
+  const customerId = watch("customer_id");
+
+  useEffect(() => {
+    if (customerId) {
+      const selectedCustomer = customers.find((c) => c.id === customerId);
+      if (selectedCustomer) {
+        setValue("customers", {
+          name: selectedCustomer.name,
+          rtn: selectedCustomer.rtn,
+          email: selectedCustomer.email,
+        });
+      }
+    }
+  }, [customerId, customers, setValue]);
+
+  const generateNextInvoiceNumber = (lastNumber: string) => {
+    const parts = lastNumber.split("-");
+    const lastPart = parts[parts.length - 1];
+    const nextNumber = (parseInt(lastPart, 10) + 1).toString().padStart(8, "0");
+    parts[parts.length - 1] = nextNumber;
+    return parts.join("-");
+  };
+
+  const onSubmit = (data: Invoice) => {
+    // return console.log(data);
+    const { subtotal, total, tax } = invoiceService.computeInvoiceData(
+      data.invoice_items,
+    );
+    data.subtotal = subtotal;
+    data.total = total;
+    data.tax = tax;
+    if (data.invoice_items.length < 1)
+      setError("invoice_items", {
+        type: "required",
+        message: "At least one invoice",
+      });
+    onSave(data);
+  };
+
+  const validateInvoiceNumber = (value: string) => {
+    const previousInvoiceNumber = lastInvoiceNumber as string;
+    const nextInvoiceNumber = value;
+    const regex = /^\d{3}-\d{3}-\d{2}-\d{8}$/;
+    if (!regex.test(value)) {
+      return "El número de factura debe tener el formato 000-000-00-00000000";
+    }
+    if (!invoiceService.isInvoiceNumberValid(value, company!.range_invoice2!))
+      return "El número de factura está fuera del rango de facturación actual";
+    const nextAndPreviousComparison = invoiceService.compareInvoiceNumbers(
+      nextInvoiceNumber,
+      previousInvoiceNumber,
+    );
+    if (!lastInvoiceExists) {
+      const nextGreaterOrEqualThanPrevious =
+        nextAndPreviousComparison === "first greater than second" ||
+        nextAndPreviousComparison === "equal";
+      if (!nextGreaterOrEqualThanPrevious) {
+        return "El siguiente número de factura no puede ser menor que el anterior";
+      }
+    }
+    if (lastInvoiceExists) {
+      const nextGreaterThanPrevious =
+        nextAndPreviousComparison === "first greater than second";
+      if (!nextGreaterThanPrevious)
+        return "El siguiente número de factura debe ser mayor que el anterior";
+    }
+    return true;
+  };
+
+  const renderEditableContent = () => (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid gap-4">
+          <Controller
+            name="customer_id"
+            control={control}
+            rules={{ required: "Debes asociar un cliente a tu factura" }}
+            render={({ field }) => (
+              <Select onValueChange={field.onChange} value={field.value}>
+                <SelectTrigger>
+                  <SelectValue placeholder="Seleccione cliente" />
+                </SelectTrigger>
+                <SelectContent>
+                  {customers.map((customer) => (
+                    <SelectItem key={customer.id} value={customer.id}>
+                      {customer.name}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            )}
+          />
+          <Button
+            type="button"
+            className="md:w-1/2"
+            onClick={() => setIsAddClientDialogOpen(true)}
+          >
+            Añadir cliente
+          </Button>
+          {errors.customer_id && (
+            <p className="text-red-500 text-sm">{errors.customer_id.message}</p>
+          )}
+          <Controller
+            name="date"
+            control={control}
+            render={({ field }) => (
+              <DatePicker
+                onChange={(date) => field.onChange(date?.toISOString())}
+              />
+            )}
+          />
+          <div className="flex flex-col sm:flex-row gap-4">
+            <div className="flex-1">
+              <div className="flex flex-col gap-2">
+                <Label className="whitespace-nowrap">Última factura</Label>
+                <Input value={lastInvoiceNumber} disabled />
+              </div>
+            </div>
+            <div className="flex-1">
+              <div className="flex flex-col gap-2">
+                <Label className="whitespace-nowrap">Nueva factura</Label>
+                <InputMask
+                  className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+                  mask="___-___-__-________"
+                  replacement={{ _: /\d/ }}
+                  {...register("invoice_number", {
+                    required: "Este campo es requerido",
+                    validate: validateInvoiceNumber,
+                  })}
+                  placeholder="000-000-00-00000000"
+                />
+                {errors.invoice_number && (
+                  <p className="text-red-500 text-sm">
+                    {errors.invoice_number.message}
+                  </p>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+        <Separator className="my-4" />
+        <div className="grid gap-4">
+          <div className="hidden sm:block">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Producto</TableHead>
+                  <TableHead>Descripción</TableHead>
+                  <TableHead>Cantidad</TableHead>
+                  <TableHead>Precio</TableHead>
+                  <TableHead>Descuento</TableHead>
+                  <TableHead>Total</TableHead>
+                  <TableHead></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {fields.map((item, index) => (
+                  <>
+                    <TableRow key={item.id}>
+                      <TableCell>
+                        <ProductSelect
+                          index={index}
+                          products={products}
+                          control={control}
+                          setValue={setValue}
+                        />
+                      </TableCell>
+                      <TableCell>
+                        <DescriptionInput index={index} control={control} />
+                      </TableCell>
+                      <TableCell>
+                        <QuantityInput index={index} control={control} />
+                      </TableCell>
+                      <TableCell>
+                        <UnitCostInput index={index} control={control} />
+                      </TableCell>
+                      <TableCell>
+                        <DiscountInput index={index} control={control} />
+                      </TableCell>
+                      <TableCell>
+                        {`Lps. ${calculateItemTotal(index, watchInvoiceItems)}`}
+                      </TableCell>
+                      <TableCell>
+                        <Button
+                          type="button"
+                          variant="destructive"
+                          size="icon"
+                          onClick={() => remove(index)}
+                        >
+                          <Trash2 className="h-4 w-4" />
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  </>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+          <div className="sm:hidden">
+            {fields.map((item, index) => (
+              <div key={item.id} className="mb-4 p-4 border rounded">
+                <ProductSelect
+                  index={index}
+                  products={products}
+                  control={control}
+                  setValue={setValue}
+                />
+                <DescriptionInput index={index} control={control} />
+                <QuantityInput index={index} control={control} />
+                <UnitCostInput index={index} control={control} />
+                <DiscountInput index={index} control={control} />
+                <div className="mt-2">
+                  {`Total: Lps. ${calculateItemTotal(index, watchInvoiceItems)}`}
+                </div>
+                <Button
+                  type="button"
+                  variant="destructive"
+                  size="sm"
+                  onClick={() => remove(index)}
+                  className="mt-2"
+                >
+                  Quitar
+                </Button>
+              </div>
+            ))}
+          </div>
+          <Button
+            type="button"
+            onClick={() =>
+              append({
+                id: "",
+                invoice_id: "",
+                product_id: "",
+                description: "",
+                quantity: 1,
+                unit_cost: 0,
+                discount: 0,
+                created_at: new Date().toISOString(),
+                updated_at: new Date().toISOString(),
+              })
+            }
+          >
+            Agregar Producto o Servicio
+          </Button>
+        </div>
+        {/* Inhabilita el botón si el formulario no se ha tocado y todas las entradas aún son inválidas */}
+        <Button
+          type="submit"
+          className="mt-4"
+          // disabled={noProductsAdded && (!isDirty || !isValid)}
+          disabled={noProductsAdded}
+        >
+          Generar Factura
+        </Button>
+      </form>
+      <Dialog
+        open={isAddClientDialogOpen}
+        onOpenChange={setIsAddClientDialogOpen}
+      >
+        <DialogTrigger asChild></DialogTrigger>
+        <DialogContent className="w-[90%]" id="contenido del dialogo papa">
+          <div className="transition-all duration-300 ease-in-out">
+            <CustomerForm
+              customer={undefined}
+              onSubmit={handleFormSubmit}
+              onCancel={() => {
+                setIsAddClientDialogOpen(false);
+              }}
+            />
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+
+  return (
+    <Card className="card-invoice overflow-hidden border-none shadow-none rounded-sm">
+      <CardHeader className="flex flex-row items-start justify-between bg-muted/50">
+        <div className="grid gap-0.5">
+          <CardTitle className="group flex items-center gap-2 text-lg">
+            Crear Factura
+          </CardTitle>
+          <CardDescription>
+            Fecha:{new Date().toLocaleDateString()}
+          </CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent className="p-6 text-sm">
+        {renderEditableContent()}
+      </CardContent>
+      <CardFooter className="flex flex-row items-center justify-between border-t bg-muted/50 px-6 py-3">
+        <div className="text-xs text-muted-foreground">
+          Factura creada:{" "}
+          <time dateTime={watch("created_at")} suppressHydrationWarning>
+            {new Date(watch("created_at")).toLocaleString()}
+          </time>
+        </div>
+        <div>
+          <Badge variant={watch("is_proforma") ? "outline" : "secondary"}>
+            {watch("is_proforma") ? "Proforma" : "Factura"}
+          </Badge>
+          <Badge variant={watch("is_proforma") ? "outline" : "secondary"}>
+            {getStatusBadge(watch("status"))}
+          </Badge>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
+
+const calculateItemTotal = (index: number, items: InvoiceItem[]) => {
+  const item = items[index];
+  return (item.quantity * item.unit_cost - item.discount).toFixed(2);
+};
+
+export default InvoiceForm;
