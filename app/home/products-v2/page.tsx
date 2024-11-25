31e41d80d245e9ccc1f0910d8732ab032a446210
CamiloTello002 <camilojtello5@gmail.com>
2024-11-25 14:53:35 -0500

    feat: Add initial code for Excel uploading

--- app/home/products-v2/page.tsx
+++ app/home/products-v2/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import { Upload } from "lucide-react";
+import React, { useCallback, useEffect, useState } from "react";
 import { ColDef } from "ag-grid-community";
 import { Package } from "lucide-react";
 import { useToast } from "@/components/ui/use-toast";
@@ -9,8 +10,19 @@ import GenericEmptyState from "@/components/molecules/GenericEmptyState";
 import { ProductForm } from "@/components/molecules/ProductForm";
 import { Dialog } from "@radix-ui/react-dialog";
 import { DataGrid } from "@/components/molecules/DataGrid";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import useUploadXls from "@/hooks/useUploadXls";
+import { Button } from "@/components/ui/button";
+import { DialogContent } from "@/components/ui/dialog";
 
 export default function ProductsPage() {
+  const {
+    handleXlsFileUpload,
+    xlsFile,
+    isAddProductsWithSpreadsheetDialogOpen,
+    setIsAddProductsWithSpreadsheetDialogOpen,
+  } = useUploadXls();
   const { products, setProducts } = useProductsStore();
   const [selectedProduct, setSelectedProduct] = useState<Product | undefined>();
   const [error, setError] = useState<string | null>(null);
@@ -22,21 +34,24 @@ export default function ProductsPage() {
   const [totalProducts, setTotalProducts] = useState(0);
   const { toast } = useToast();
 
-  const loadProducts = async (page: number, size: number) => {
-    try {
-      const response = await productService.getProductsPaginated(page, size);
-      setProducts(response.data);
-      setTotalProducts(response.total);
-    } catch (err) {
-      console.error("Error loading products:", err);
-      toast({
-        title: "Error",
-        description:
-          "No se pudieron cargar los productos. Por favor, intente de nuevo.",
-        variant: "destructive",
-      });
-    }
-  };
+  const loadProducts = useCallback(
+    async (page: number, size: number) => {
+      try {
+        const response = await productService.getProductsPaginated(page, size);
+        setProducts(response.data);
+        setTotalProducts(response.total);
+      } catch (err) {
+        console.error("Error loading products:", err);
+        toast({
+          title: "Error",
+          description:
+            "No se pudieron cargar los productos. Por favor, intente de nuevo.",
+          variant: "destructive",
+        });
+      }
+    },
+    [setProducts, setTotalProducts, toast],
+  );
 
   const columnDefs: ColDef<Product>[] = [
     {
@@ -96,7 +111,7 @@ export default function ProductsPage() {
 
   useEffect(() => {
     loadProducts(currentPage, pageSize);
-  }, [currentPage, pageSize]);
+  }, [currentPage, pageSize, loadProducts]);
 
   const handlePageChange = (params: any) => {
     setCurrentPage(params.page);
@@ -151,8 +166,61 @@ export default function ProductsPage() {
           )}
         </main>
       </div>
+      <section className="px-16 flex flex-col gap-5 pb-16">
+        <h2 className="text-xl font-bold">
+          ¿Tienes una tabla de Excel con tus productos? Añádelos aquí{" "}
+        </h2>
+        <Label
+          htmlFor="xls"
+          className="mt-2 inline-block cursor-pointer w-[60%] mx-auto"
+        >
+          <div className="flex flex-col gap-4 mx-auto w-full border-2 border-dashed border-gray-300 rounded-lg p-6 text-center">
+            <Upload className="mx-auto h-12 w-12 text-gray-400" />
+            <p className="mt-1">
+              Arrastra y suelta tu archivo de Excel, o dale click en este
+              recuadro para seleccionar un archivo
+            </p>
+            <Input
+              id="xls"
+              name="xls"
+              type="file"
+              accept=".xlsx,.xls,.csv"
+              className="hidden"
+              onChange={handleXlsFileUpload}
+            />
+            {xlsFile && xlsFile.length > 0 && (
+              <table className="table">
+                <thead>
+                  <tr>
+                    {Object.keys(xlsFile[0]).map((key) => (
+                      <th key={key}>{key}</th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {xlsFile &&
+                    xlsFile.map((row, index) => (
+                      <tr key={index}>
+                        {Object.values(row).map((value, index) => (
+                          <td key={index}>{value.toString()}</td>
+                        ))}
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            )}
+          </div>
+        </Label>
+      </section>
+      {/* <Button onClick={() => console.log(xlsFile)}>Mirar datossss</Button> */}
       <Dialog open={isDeleteDialogOpen} onOpenChange={setIsDeleteDialogOpen}>
         {/* Dialog content */}
+      </Dialog>
+      <Dialog
+        open={isAddProductsWithSpreadsheetDialogOpen}
+        onOpenChange={setIsAddProductsWithSpreadsheetDialogOpen}
+      >
+        <DialogContent className="w-3/5">hola a todos</DialogContent>
       </Dialog>
     </div>
   );
