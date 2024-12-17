ee8b0a6c6a53f2b2c45b3da4a2caeedd42b3b548
CamiloTello002 <camilojtello5@gmail.com>
2024-12-16 22:54:27 -0500

    feat: Create service for searching product

--- lib/supabase/services/product.ts
+++ lib/supabase/services/product.ts
@@ -21,22 +21,22 @@ class ProductService extends BaseService {
 
   async getProductsPaginated(
     page: number,
-    pageSize: number
+    pageSize: number,
   ): Promise<PaginatedResponse<Product>> {
     return this.getAllPaginated<Product>(this.tableName, { page, pageSize });
   }
   async createProduct(
-    product: Omit<Product, "id" | "company_id" | "created_at" | "updated_at">
+    product: Omit<Product, "id" | "company_id" | "created_at" | "updated_at">,
   ): Promise<Product | null> {
     return this.create<Product>(this.tableName, product);
   }
 
   async createMultipleProducts(
-    products: Record<string, any>[]
+    products: Record<string, any>[],
   ): Promise<{ success: boolean; message: string }> {
     const response = await this.createMultiple<Product>(
       this.tableName,
-      products
+      products,
     );
     return response;
   }
@@ -51,14 +51,14 @@ class ProductService extends BaseService {
 
   async updateProduct(
     id: string,
-    updates: Partial<Product>
+    updates: Partial<Product>,
   ): Promise<Product | null> {
     return this.update<Product>(this.tableName, id, updates);
   }
 
   async updateMultipleProducts(
     // id: string,
-    updates: Product[]
+    updates: Product[],
   ): Promise<{ success: boolean; message: string }> {
     return this.updateMultiple<Product>(this.tableName, updates);
   }
@@ -71,13 +71,27 @@ class ProductService extends BaseService {
     return this.search<Product>(this.tableName, "description", query);
   }
 
+  async searchProductsByCompany(
+    searchString: string,
+  ): Promise<Pick<Product, "id" | "unit_cost" | "description">[]> {
+    const companyId = await this.ensureCompanyId();
+    if (!companyId) return [];
+    const { data, error } = await this.supabase
+      .from(this.tableName)
+      .select("*")
+      .eq("company_id", companyId)
+      .or(`sku.ilike.%${searchString}%,description.ilike.%${searchString}%`);
+    if (error || !data) return [];
+    return data;
+  }
+
   async updateInventory(productId: string, quantity: number): Promise<boolean> {
     const { data, error } = await this.supabase.rpc(
       "update_product_inventory",
       {
         product_id: productId,
         quantity_change: quantity,
-      }
+      },
     );
 
     if (error) {
