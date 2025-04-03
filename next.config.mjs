380bbc0c92eb7d57bfa734d42c123111f2daddde
kenystev <kjestevez.0795@gmail.com>
2025-04-03 10:34:57 -0700

    Fix redirect route / to /home

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ['@react-pdf/renderer'],
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
       },
       {
-        protocol: "https",
-        hostname: "vksuzavozvxpwmzjjmcz.supabase.co",
+        protocol: 'https',
+        hostname: 'vksuzavozvxpwmzjjmcz.supabase.co',
       },
       {
-        protocol: "https",
-        hostname: "zgbcahdtatfpxovaetoj.supabase.co",
+        protocol: 'https',
+        hostname: 'zgbcahdtatfpxovaetoj.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/sign/**',
       },
     ],
   },
   async redirects() {
     return [
       {
-        source: "/",
-        destination: "/home",
+        source: '/',
+        destination: '/home',
         permanent: true,
       },
     ];
